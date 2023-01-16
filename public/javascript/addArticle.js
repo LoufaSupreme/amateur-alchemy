56ea3a9cd171608712bf6987bac1e35ed211ab48
josh <josh.davis@live.ca>
2023-01-15 22:01:36 -0500

    improved placeholder article text

--- public/javascript/addArticle.js
+++ public/javascript/addArticle.js
@@ -1,19 +1,21 @@
 const htmlPreview = `\
 <style>
-  
+
 </style>
-<div>
-  <figure class="float-right fig-small">
-      <img  src="/uploads/b62f70af-e190-4eb6-83a2-23db7b9b2bd9.jpeg" alt="canoeing">
-      <figcaption>We out here we finna bust</figcaption>
-  </figure>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum lacinia dui at sagittis. Etiam suscipit erat dapibus, dictum eros vitae, pharetra tellus. Mauris pellentesque venenatis metus. Maecenas iaculis malesuada odio at sollicitudin. Donec vulputate convallis semper. Cras pretium sit amet elit sed laoreet. Duis porta tristique nibh. Maecenas imperdiet molestie tellus, ut aliquet nisi ornare vitae. Nunc auctor tristique turpis nec tristique. Pellentesque blandit lectus eu est consectetur mattis non eget quam. In nec cursus elit, quis dictum augue.
-  </p>
-  <h2>Brewing Dank Ass Beers</h2>
-  <p>
-    Vivamus at tempor odio. Pellentesque posuere, mi in tempus vehicula, eros risus luctus dolor, condimentum commodo mauris mauris sit amet nulla. Cras tincidunt pharetra turpis nec congue. Sed ornare consectetur facilisis. Mauris ultrices vulputate lacus, ac fermentum risus porttitor in. Vivamus varius rutrum enim. Pellentesque varius, augue vel tristique hendrerit, dolor lorem fringilla diam, vitae luctus ligula mauris quis turpis. Proin suscipit elit non ante dignissim, id pharetra velit iaculis. Pellentesque ac libero pellentesque, suscipit orci et, posuere risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec euismod dui, eget laoreet diam. In pretium quis diam nec mattis. In ultrices nec magna vel tincidunt. Fusce ex quam, eleifend sit amet egestas quis, vehicula a ex.
-  </p>
+<div class="flex column">
+  <section>
+    <figure class="float-right fig-small">
+        <img  src="/uploads/b62f70af-e190-4eb6-83a2-23db7b9b2bd9.jpeg" alt="canoeing">
+        <figcaption>We out here we finna bust</figcaption>
+    </figure>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum lacinia dui at sagittis. Etiam suscipit erat dapibus, dictum eros vitae, pharetra tellus. Mauris pellentesque venenatis metus. Maecenas iaculis malesuada odio at sollicitudin. Donec vulputate convallis semper. Cras pretium sit amet elit sed laoreet. Duis porta tristique nibh. Maecenas imperdiet molestie tellus, ut aliquet nisi ornare vitae. Nunc auctor tristique turpis nec tristique. Pellentesque blandit lectus eu est consectetur mattis non eget quam. In nec cursus elit, quis dictum augue.
+    </p>
+    <h2>Brewing Dank Ass Beers</h2>
+    <p>
+      Vivamus at tempor odio. Pellentesque posuere, mi in tempus vehicula, eros risus luctus dolor, condimentum commodo mauris mauris sit amet nulla. Cras tincidunt pharetra turpis nec congue. Sed ornare consectetur facilisis. Mauris ultrices vulputate lacus, ac fermentum risus porttitor in. Vivamus varius rutrum enim. Pellentesque varius, augue vel tristique hendrerit, dolor lorem fringilla diam, vitae luctus ligula mauris quis turpis. Proin suscipit elit non ante dignissim, id pharetra velit iaculis. Pellentesque ac libero pellentesque, suscipit orci et, posuere risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec euismod dui, eget laoreet diam. In pretium quis diam nec mattis. In ultrices nec magna vel tincidunt. Fusce ex quam, eleifend sit amet egestas quis, vehicula a ex.
+    </p>
+  </section>
   <div class="sidebyside flex">
     <figure>
       <img  src="/uploads/ab48b59b-d0ca-46e5-85a1-cc3d91d5e8a9.jpeg" alt="tanks">
@@ -24,16 +26,18 @@ const htmlPreview = `\
         <figcaption>We out here we finna bust</figcaption>
     </figure>
   </div>
-  <p>
-    Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat. Etiam consectetur, nisi et gravida placerat, neque 
-  </p>
-  <figure class="float-left fig-small">
-      <img  src="/uploads/b62f70af-e190-4eb6-83a2-23db7b9b2bd9.jpeg" alt="canoeing">
-      <figcaption>We out here we finna bust</figcaption>
-  </figure>
-  <p>
-    Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat. Etiam consectetur, nisi et gravida placerat, neque mauris congue dui, sit amet semper lacus mi a leo. Nunc ac nisl elit. Duis sed sapien pulvinar, dapibus libero ac, pulvinar velit. Nulla at lorem elit. Aenean laoreet sodales ipsum nec hendrerit. Maecenas ut urna cursus, porta sapien eu, rhoncus ligula. 
-  </p>
+  <section>
+    <p>
+      Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat. Etiam consectetur, nisi et gravida placerat, neque 
+    </p>
+    <figure class="float-left fig-small">
+        <img  src="/uploads/b62f70af-e190-4eb6-83a2-23db7b9b2bd9.jpeg" alt="canoeing">
+        <figcaption>We out here we finna bust</figcaption>
+    </figure>
+    <p>
+      Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat. Etiam consectetur, nisi et gravida placerat, neque mauris congue dui, sit amet semper lacus mi a leo. Nunc ac nisl elit. Duis sed sapien pulvinar, dapibus libero ac, pulvinar velit. Nulla at lorem elit. Aenean laoreet sodales ipsum nec hendrerit. Maecenas ut urna cursus, porta sapien eu, rhoncus ligula. 
+    </p>
+  </section>
   <blockquote>
     "Block quote. Block quote. Block quote. Block quote."
   </blockquote>
@@ -42,14 +46,16 @@ const htmlPreview = `\
       <figcaption>We out here we finna bust</figcaption>
   </figure>
   <h2>Subheadings R' Us</h2>
-  <aside class="float-right">
+  <section>
+    <aside class="float-right">
+      <p>
+      Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat.
+      </p>
+    </aside>
     <p>
-    Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat.
+      Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat. Etiam consectetur, nisi et gravida placerat, neque mauris congue dui, sit amet semper lacus mi a leo. Nunc ac nisl elit. Duis sed sapien pulvinar, dapibus libero ac, pulvinar velit. Nulla at lorem elit. Aenean laoreet sodales ipsum nec hendrerit. Maecenas ut urna cursus, porta sapien eu, rhoncus ligula. 
     </p>
-  </aside>
-  <p>
-    Suspendisse sapien risus, venenatis sed ultrices eget, facilisis vitae sem. Nam eget lectus ut elit vestibulum dapibus. Praesent pretium eleifend consequat. Etiam consectetur, nisi et gravida placerat, neque mauris congue dui, sit amet semper lacus mi a leo. Nunc ac nisl elit. Duis sed sapien pulvinar, dapibus libero ac, pulvinar velit. Nulla at lorem elit. Aenean laoreet sodales ipsum nec hendrerit. Maecenas ut urna cursus, porta sapien eu, rhoncus ligula. 
-  </p>
+  </section>
   <table class="styled-table">
     <thead>
       <tr>
@@ -74,9 +80,7 @@ const htmlPreview = `\
       </tr>
     </tbody>
   </table>
-</div>
-
-\ 
+</div>\ 
 `;
 const preview = document.querySelector('.preview');
 const hiddenBodyInput = document.querySelector('#body');
