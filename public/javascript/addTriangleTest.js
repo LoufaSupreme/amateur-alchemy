1c962d1b9de43a1745981983d64784f0b3cfabcc
josh <josh.davis@live.ca>
2023-01-24 00:15:10 -0500

    fixed edge cases on addTriangleTest form

--- public/javascript/addTriangleTest.js
+++ public/javascript/addTriangleTest.js
@@ -4,20 +4,25 @@ const perceivedUniqueRadios = document.querySelectorAll('input[name="perceived_u
 const preferenceRadios = document.querySelectorAll('input[name="preference"]');
 
 const preferenceContainer = document.querySelector('.preference');
-const preferenceBeerLetterSpans = document.querySelectorAll('.preference-beer-letter');
-const notPreferenceBeerLetterSpans = document.querySelectorAll('.not-preference-beer-letter');
+const uniqueBeerLetterSpans = document.querySelectorAll('.unique-beer-letter');
+const notUniqueBeerLetterSpans = document.querySelectorAll('.not-unique-beer-letter');
 
 const compareContainer = document.querySelector('.compare');
 const scoresContainer = document.querySelector('.scores');
+const scores = document.querySelectorAll('.score');
 
 const detectFlawsContainer = document.querySelector('#detect-flaws-container');
 const flawsDetected = document.querySelector('#flaws-true');
 const flawsNotDetected = document.querySelector('#flaws-false');
-const flawsContainer = document.querySelector('#off-flavor-container');
+const flawsContainer = document.querySelector('#flaws');
+const flawsTitle = document.querySelector('#flaws__title');
 const flawInputs = document.querySelectorAll('.flaw__input');
 
 const rangeInputs = document.querySelectorAll('input[type=range]');
 const rangeInputSubheadings = document.querySelectorAll('.range-input__subheading');
+
+
+let chosenUniqueBeer;
 
 /////// event listeners
 
@@ -41,30 +46,115 @@ rangeInputs.forEach(range => {
 // fires when a user picks a response for "which beer is unique"
 function handlePerceivedUniqueChange(e) {
     const perceivedUniqueBeer = e.target.value;
+    chosenUniqueBeer = perceivedUniqueBeer;
+
     const letterOptions = ['A', 'B', 'C'];
 
     // changing the spans to show which beer was chosen as preferred
-    preferenceBeerLetterSpans.forEach(span => span.innerText = perceivedUniqueBeer);
+    uniqueBeerLetterSpans.forEach(span => span.innerText = perceivedUniqueBeer);
     
+    // update dataset b/c its used to render the subheading
     rangeInputSubheadings.forEach(subheading => subheading.dataset.unique = perceivedUniqueBeer);
 
     // do same for the spans for the other beers
-    notPreferenceBeerLetterSpans.forEach(span => span.innerText = letterOptions.filter(letter => letter != perceivedUniqueBeer).join(' & '));
+    notUniqueBeerLetterSpans.forEach(span => span.innerText = letterOptions.filter(letter => letter != perceivedUniqueBeer).join(' & '));
 
+    // show the preference and flaws containers
     preferenceContainer.classList.add('active');
+
+    // show the did you detect flaws container:
     detectFlawsContainer.classList.add('active');
+
+    // show the rate beers inputs (stars):
+    scoresContainer.classList.add('active');
 }
 
 // fires when a user picks a preference option
 function handlePreferenceChange(e) {
     const preference = e.target.value;
     if (preference !== "none") {
+
+        // show the compare beers range inputs:
         compareContainer.classList.add('active');
-        scoresContainer.classList.add('active');
+
+        // reset the off-flavour (flaws) title
+        flawsTitle.innerText = "Match the off-flavour to the beer:";
+
+        // reset the flaw inputs
+        flawInputs.forEach(input => {
+            const label = document.querySelector(`label[for="${input.id}"]`);
+            const labelIconDiv = label.querySelector('.icon');
+            const labelTextDiv = label.querySelector('.text');
+
+            if (input.classList.contains("flaw__input--other")) {
+                input.style.display = "block";
+                input.checked = false;
+                label.style.display = "block";
+            }
+            else {
+                input.value = "unique";
+                input.checked = false;
+                labelIconDiv.style.display = "none";
+                labelTextDiv.style.display = "block";
+            }
+        })
+
+        // reset the score ratings
+        scores.forEach(score => {
+            if (score.id === 'score__other') score.style.display = "grid";
+            else {
+                const label = score.querySelector('.scores__beer-label');
+                const labelDefaultText = label.querySelector('.default');
+                const labelAlternateText = label.querySelector('.alternate');
+
+                labelDefaultText.style.display = "block";
+                labelAlternateText.style.display = "none";
+            }
+        })
     }
+    // if the user selects no perceivable difference between beers:
     else {
+        // hide the compare range inputs
         compareContainer.classList.remove('active');
-        scoresContainer.classList.remove('active');
+
+        // modify the off-flavour title to make sense for 1 beer
+        flawsTitle.innerText = "Select the off-flavours you detected:";
+
+        // reset range inputs
+        resetComparisonRanges();
+        resetComparisonRangeSubheadings();
+
+        // change the flaw inputs to just one input
+        flawInputs.forEach(input => {
+            const label = document.querySelector(`label[for="${input.id}"]`);
+            const labelIconDiv = label.querySelector('.icon');
+            const labelTextDiv = label.querySelector('.text');
+
+            if (input.classList.contains("flaw__input--other")) {
+                input.style.display = "none"
+                input.checked = false;
+                label.style.display = "none";
+            }
+            else {
+                input.value = "all";
+                labelIconDiv.style.display = "block";
+                labelTextDiv.style.display = "none";
+                input.checked = false;
+            }
+        })
+
+        // hide one of the score ratings
+        scores.forEach(score => {
+            if (score.id === 'score__other') score.style.display = "none";
+            else {
+                const label = score.querySelector('.scores__beer-label');
+                const labelDefaultText = label.querySelector('.default');
+                const labelAlternateText = label.querySelector('.alternate');
+
+                labelDefaultText.style.display = "none";
+                labelAlternateText.style.display = "block";
+            }
+        })
     }
 }
 
@@ -76,6 +166,18 @@ function handleFlawsDetected(e) {
     else flawsContainer.classList.remove('active');
 }
 
+// reset all range inputs back to default
+function resetComparisonRanges(val = 2) {
+    rangeInputs.forEach(input => input.value = val);
+}
+
+// reset all range input subheadings
+function resetComparisonRangeSubheadings() {
+    rangeInputSubheadings.forEach(subheading => {
+        subheading.innerText = `The ${subheading.dataset.attr} is about equal.`;
+    });
+}
+
 // fires when the user changes a range input in the compare section
 function handleRangeChange(e) {
     const inputParent = e.target.closest('.range-input');
@@ -85,6 +187,7 @@ function handleRangeChange(e) {
     const uniqueBeer = subheading.dataset.unique;
     const otherBeers = letterOptions.filter(letter => letter != uniqueBeer).join(' & ');
 
+    // update subheading:
     switch(e.target.value) {
         case '0':
             subheading.innerText = `Beer ${uniqueBeer} is MUCH higher in ${subheading.dataset.attr} than Beers ${otherBeers}.`;
