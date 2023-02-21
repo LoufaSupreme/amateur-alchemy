f85c2c43f3ccd31a77acb037b694df1ff4b5f8c3
josh <josh.davis@live.ca>
2023-02-20 23:40:37 -0500

    style improvements for mobile

--- public/javascript/articleQrCode.js
+++ public/javascript/articleQrCode.js
@@ -1,11 +1,14 @@
 const qrContainer = document.querySelector('#qr-code');
 const link = document.querySelector('.url');
+const qr_link = document.querySelector('.qr-url');
 const article_num = link.dataset.article_num;
 
 // set url span
 const targetURL = `${window.location.protocol}${window.location.host}/articles/${article_num}/triangle-test`;
 link.innerText = targetURL;
 link.href = `/articles/${article_num}/triangle-test`;
+qr_link.href = `/articles/${article_num}/triangle-test`;
 
 // generate QR Code
+// uses https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js
 new QRCode(qrContainer, targetURL)
